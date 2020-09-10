68e6178925b604e586b72ebfd04bfcf4f1a6c451
JEgor <kafder97@gmail.com>
2020-09-10 07:48:29 +0300

    4 этап Canvas, выполнен

--- canvas/JS/game.js
+++ canvas/JS/game.js
@@ -1,9 +1,15 @@
 let game = {
 	ctx: null,
+	platform: null,
+	ball: null,
+	blocks: [],
+	rows: 4,
+	cols: 8,
 	sprites: {
 		background: null,
 		ball: null,
 		platform: null,
+		block: null
 	},
 	init: function() {
 		this.ctx = document.getElementById("mycanvas").getContext("2d");
@@ -11,16 +17,27 @@ let game = {
 	preload(callback) {
 		let loaded = 0;
 		let required = Object.keys(this.sprites).length;
+		let onImageLoad = () => {
+			++loaded;
+			if (loaded >= required) {
+				callback();
+			}
+		};
 
 		for (let key in this.sprites) {
 			this.sprites[key] = new Image();
 			this.sprites[key].src = "media/image/" + key + ".png";
-			this.sprites[key].addEventListener("load", () => {
-				++loaded;
-				if (loaded >= required) {
-					callback();
-				}
-			});
+			this.sprites[key].addEventListener("load", onImageLoad);
+		}
+	},
+	create() {
+		for (let row = 0; row < this.rows; row++) {
+			for (let col = 0; col < this.cols; col++) {
+				this.blocks.push({
+					x: 64 * col + 65,
+					y: 24 * row + 35
+				});
+			}
 		}
 	},
 	run() {
@@ -29,9 +46,10 @@ let game = {
 		});
 	},
 	render() {
-		this.ctx.drawImage(this.sprites.background, 0	, 0);
-		this.ctx.drawImage(this.sprites.ball, 0, 0);
-		this.ctx.drawImage(this.sprites.platform, 0, 0);
+		this.ctx.drawImage(this.sprites.background, 0, 0);
+		this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+			this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
 	},
 	start: function() {
 		this.init();
