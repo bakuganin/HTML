61f156fdeed9804bb24ff5c48fcdbff481d07343
JEgor <kafder97@gmail.com>
2020-09-14 09:01:31 +0300

    Добавлена разделение по классам и апгрейт графики

--- canvas/JS/game.js
+++ canvas/JS/game.js
@@ -11,10 +11,10 @@ let game = {
 	ball: null,
 	blocks: [],
 	score: 0,
-	rows: 4,
-	cols: 8,
-	width: 640,
-	height: 360,
+	rows: 6,
+	cols: 1,
+	width: 1280,
+	height: 720,
 	sprites: {
 		background: null,
 		ball: null,
@@ -24,8 +24,19 @@ let game = {
 	sounds: {
 		bump: null,
 	},
+	initCanvasSize() {
+		let realWidth = window.innerWidth * window.devicePixelRatio;
+		let realHeight = window.innerHeight * window.devicePixelRatio;
+		let maxHeight = this.height;
+		let maxWidth = this.width;
+		this.height = Math.min(Math.floor(maxWidth * realHeight / realWidth), maxHeight);
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+	},
 	init: function() {
-		this.ctx = document.getElementById("mycanvas").getContext("2d");
+		this.canvas = document.getElementById("mycanvas");
+		this.ctx = this.canvas.getContext("2d");
+		this.initCanvasSize();
 		this.setTextFont();
 		this.setEvents();
 	},
@@ -63,25 +74,30 @@ let game = {
 	preloadSprites(onResourceLoad) {
 		for (let key in this.sprites) {
 			this.sprites[key] = new Image();
-			this.sprites[key].src = "media/image/" + key + ".png";
+			this.sprites[key].src = "media/update/sprites/" + key + ".png";
 			this.sprites[key].addEventListener("load", onResourceLoad);
 		}
 	},
 	preloadAudio(onResourceLoad) {
 		for (let key in this.sounds) {
-			this.sounds[key] = new Audio("media/sounds/" + key + ".mp3");
+			this.sounds[key] = new Audio("media/update/sounds/" + key + ".mp3");
 			this.sounds[key].addEventListener("canplaythrough", onResourceLoad, {once: true});
 		}
 	},
 	create() {
+		this.ball.x = this.width / 2 - 20;
+		this.ball.y = this.height - 85;
+		this.platform.x = this.width / 2 - 125;
+		this.platform.y = this.height - 45;
+
 		for (let row = 0; row < this.rows; row++) {
 			for (let col = 0; col < this.cols; col++) {
 				this.blocks.push({
 					active: true,
-					width: 60,
-					height: 20,
-					x: 64 * col + 65,
-					y: 24 * row + 35
+					width: 111,
+					height: 39,
+					x: 113 * col + 70,
+					y: 42 * row + 90
 				});
 			}
 		}
@@ -155,146 +171,6 @@ let game = {
 	},
 	random(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
-};
-
-game.ball = {
-	dx: 0,
-	dy: 0,
-	frame: 0,
-	velocity: 3,
-	x: 320,
-	y: 280,
-	width: 20,
-	height: 20,
-	start() {
-		this.dy = -this.velocity;
-		this.dx = game.random(-this.velocity, this.velocity);
-		this.animate();
-	},
-	animate() {
-		setInterval(() => {
-			++this.frame;
-			if (this.frame > 3) {
-				this.frame = 0;
-			}
-		}, 100);
-	},
-	move() {
-		if (this.dy) {
-			this.y += this.dy;
-		}
-		if (this.dx) {
-			this.x += this.dx;
-		}
-	},
-	collide(element) {
-		let x = this.x + this.dx;
-		let y = this.y + this.dy;
-
-		if (x + this.width > element.x &&
-			x < element.x + element.width &&
-			y + this.height > element.y &&
-			y < element.y + element.height) {
-				return true;
-			}
-		return false;
-	},
-	collideWorldBounds() {
-		let x = this.x + this.dx;
-		let y = this.y + this.dy;
-
-		let ballLeft = x;
-		let ballRight = ballLeft + this.width;
-		let ballTop = y;
-		let ballBottom = ballTop + this.height;
-
-		let worldLeft = 0;
-		let worldRight = game.width;
-		let worldTop = 0;
-		let worldBottom = game.height;
-
-		if (ballLeft < worldLeft) {
-			this.x = 0;
-			this.dx = this.velocity;
-			game.sounds.bump.play();
-		} else if (ballRight > worldRight) {
-			this.x = worldRight - this.width;
-			this.dx = -this.velocity;
-			game.sounds.bump.play();
-		} else if (ballTop < worldTop) {
-			this.y = 0;
-			this.dy = this.velocity;
-			game.sounds.bump.play();
-		} else if (ballBottom > worldBottom) {
-			game.end("You're lost!");
-		}
-	},
-	bumpBlock(block) {
-		this.dy *= -1;
-		block.active = false;
-	},
-	bumpPlatform(platform) {
-		if (platform.dx) {
-			this.x += platform.dx;
-		}
-
-		if (this.dy > 0) {
-			this.dy = -this.velocity;
-			let touchX = this.x + this.width / 2;
-			this.dx = this.velocity * platform.getTouchOffset(touchX);
-		}
-	}
-};
-
-game.platform = {
-	velocity: 6,
-	dx: 0,
-	x: 280,
-	y: 300,
-	width: 100,
-	height: 14,
-	ball: game.ball,
-	fire() {
-		if (this.ball) {
-			this.ball.start();
-			this.ball = null;
-		}
-	},
-	start(direction) {
-		if (direction === KEYS.LEFT) {
-			this.dx = -this.velocity;
-		} else if (direction === KEYS.RIGHT) {
-			this.dx = this.velocity;
-		}
-	},
-	stop() {
-		this.dx = 0;
-	},
-	move() {
-		if (this.dx) {
-			this.x += this.dx;
-			if (this.ball) {
-				this.ball.x += this.dx;
-			}
-		}
-	},
-	getTouchOffset(x) {
-		let diff = (this.x + this.width) - x;
-		let offset = this.width - diff;
-		let result = 2 * offset / this.width;
-		return result - 1;
-	},
-	collideWorldBounds() {
-		let x = this.x + this.dx;
-		let platformLeft = x;
-		let platformRight = platformLeft + this.width;
-		let worldLeft = 0;
-		let worldRight = game.width;
-
-		if (platformLeft < worldLeft || platformRight > worldRight) {
-			this.dx = 0;
-		}
 	}
 };
 
